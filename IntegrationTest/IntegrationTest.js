c7039fd680a15bec9aa9306242fb696b6486352e
Brendan LEGENDRE <brendan.legendre@epsi.fr>
2021-02-12 20:24:02 +0100

    ..

--- IntegrationTest/IntegrationTest.js
+++ IntegrationTest/IntegrationTest.js
@@ -1,9 +1,15 @@
-function sendToApiConst() {
-  return true;
-}
+// function sendToApiConst() {
+//   return true;
+// }
 
-QUnit.module('sendToApiConst',function() {
-  QUnit.test('function', function(assert) {
-    assert.equal(false, true, true)
-  });
+// QUnit.module('sendToApiConst',function() {
+//   QUnit.test('function', function(assert) {
+//     assert.equal(false, true, true)
+//   });
+// });
+
+QUnit.module( "Group A" );
+
+QUnit.test( "basic test example 1", function( assert ) {
+  assert.true( false, "this is fine" );
 });
--- UnitTests/UnitTest.js
+++ UnitTests/UnitTest.js
@@ -1,8 +1,14 @@
-function add(a, b) {
-    return a + b;
-}
-QUnit.module('add', function() {
-    QUnit.test('should add two numbers', function(assert) {
-    assert.equal(add(1, 1), 2, '1 + 1 = 2');
-    });
+// function add(a, b) {
+//     return a + b;
+// }
+// QUnit.module('add', function() {
+//     QUnit.test('should add two numbers', function(assert) {
+//     assert.equal(add(1, 1), 2, '1 + 1 = 2');
+//     });
+// });
+
+QUnit.module( "Group A" );
+
+QUnit.test( "basic test example 1", function( assert ) {
+  assert.true( true, "this is fine" );
 });
